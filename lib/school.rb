1422f5e248bb4cf21dedd03d69c4b5df674bb375
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-09-24 22:38:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
   end
   
   def add_student(student_name, student_grade)
-  roster [:student_grade] = []
-  roster [:student_grade] << student_grade
+  roster[:student_grade] = []
+  roster[:student_grade] << student_grade
   roster
   end
 
